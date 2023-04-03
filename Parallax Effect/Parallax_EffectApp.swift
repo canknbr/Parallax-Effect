10abfe634d2d866996f0a822b766505e8e4bba25
canknbr <cankanbur1@gmail.com>
2023-04-03 16:45:42 +0300

    Initial Commit

---
+++ Parallax Effect/Parallax_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parallax_EffectApp.swift
+//  Parallax Effect
+//
+//  Created by Can Kanbur on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Parallax_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
